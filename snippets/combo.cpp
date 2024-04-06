85bea35366bb3f95825c70cab5256ef9c77dbfcc
martianbit <martianbit@proton.me>
2024-04-06 11:39:04 +0200

    add pre_pow

--- snippets/combo.cpp
+++ snippets/combo.cpp
@@ -1,4 +1,12 @@
+vector<ll> _pow;
 vector<ll> fact;
+void pre_pow(ll a, ll b) {
+    _pow.clear();
+    _pow.resize(b + 1);
+    _pow[0] = 1;
+    for(ll i = 1; i <= b; i++)
+        _pow[i] = (a * _pow[i - 1]) % MOD;
+}
 void pre_fact(ll n) {
     fact.clear();
     fact.resize(n + 1);
