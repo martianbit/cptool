8c7a7ce16cb1ff733b01463dcae75a904712567f
martianbit <martianbit@proton.me>
2024-04-19 13:58:58 +0200

    improve template

--- template.cpp
+++ template.cpp
@@ -7,8 +7,14 @@
 using namespace std;
 typedef long long ll;
 const ll INF = (1LL << 62);
+void solve() {
+}
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ll T;
+    cin >> T;
+    while(T--)
+        solve();
     return 0;
 }
 
